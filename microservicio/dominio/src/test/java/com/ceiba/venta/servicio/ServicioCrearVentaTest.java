41ede15e3ebfbd37222839a3049b18573e2d45ad
Oscar Olivo <ingoscarolivo@gmail.com>
2022-02-16 12:34:23 -0500

    nuevos test

--- microservicio/dominio/src/test/java/com/ceiba/venta/servicio/ServicioCrearVentaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/venta/servicio/ServicioCrearVentaTest.java
@@ -1,35 +1,34 @@
 package com.ceiba.venta.servicio;
 
 import com.ceiba.BasePrueba;
-import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.libro.modelo.entidad.Libro;
 import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
 import com.ceiba.libro.servicio.ServicioActualizarLibro;
-import com.ceiba.libro.servicio.ServicioCrearLibro;
-import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
 import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.venta.modelo.entidad.Venta;
 import com.ceiba.venta.puerto.repositorio.RepositorioVenta;
 import com.ceiba.venta.servicio.testdatabuilder.VentaTestDataBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.spy;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ServicioCrearVentaTest {
 
+    private RepositorioVenta repositorioVenta = Mockito.mock(RepositorioVenta.class);;
+    private RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
+    private RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
+    private ServicioActualizarLibro servicioActualizarLibro = Mockito.mock(ServicioActualizarLibro.class);
 
     @Test
     @DisplayName("Deberia lanzar una exepcion cuando se valide la existencia del libro")
     void deberiaLanzarUnaExepcionCuandoSeValideLaExistenciaDelLibro() {
-        // arrange
-        RepositorioVenta repositorioVenta = Mockito.mock(RepositorioVenta.class);
-        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
-        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
-        ServicioActualizarLibro servicioActualizarLibro = Mockito.mock(ServicioActualizarLibro.class);
         ServicioCrearVenta servicioCrearVenta =  spy(new ServicioCrearVenta(repositorioVenta, repositorioLibro, repositorioUsuario, servicioActualizarLibro));
         // act - assert
         BasePrueba.assertThrows(() -> servicioCrearVenta.validarExistenciaPreviaLibro(1L), ExcepcionValorInvalido.class,"El libro no existe en el sistema");
@@ -39,11 +38,6 @@ public class ServicioCrearVentaTest {
     @Test
     @DisplayName("Deberia lanzar una exepcion cuando se valide inventario del libro")
     void deberiaLanzarUnaExepcionCuandoSeValideInventarioDelLibro() {
-        // arrange
-        RepositorioVenta repositorioVenta = Mockito.mock(RepositorioVenta.class);
-        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
-        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
-        ServicioActualizarLibro servicioActualizarLibro = Mockito.mock(ServicioActualizarLibro.class);
         ServicioCrearVenta servicioCrearVenta =  spy(new ServicioCrearVenta(repositorioVenta, repositorioLibro, repositorioUsuario, servicioActualizarLibro));
         // act - assert
         BasePrueba.assertThrows(() -> servicioCrearVenta.validarInventarioLibro(-1L), ExcepcionValorInvalido.class,"No ha inventario del libro");
@@ -53,14 +47,18 @@ public class ServicioCrearVentaTest {
     @Test
     @DisplayName("Deberia lanzar una exepcion cuando se valide la existencia del usuario")
     void deberiaLanzarUnaExepcionCuandoSeValideLaExistenciaDelUsuario() {
-        // arrange
-        RepositorioVenta repositorioVenta = Mockito.mock(RepositorioVenta.class);
-        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
-        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
-        ServicioActualizarLibro servicioActualizarLibro = Mockito.mock(ServicioActualizarLibro.class);
         ServicioCrearVenta servicioCrearVenta =  spy(new ServicioCrearVenta(repositorioVenta, repositorioLibro, repositorioUsuario, servicioActualizarLibro));
         // act - assert
         BasePrueba.assertThrows(() -> servicioCrearVenta.validarExistenciaPreviaUsuario(1L), ExcepcionValorInvalido.class,"El usuario no existe en el sistema");
+    }
+
+    @Test
+    @DisplayName("Deberia retornar true si el precio del libro es menor o False si el precio del libro es mayor")
+    void deberiaRetornarTrueSIPrecioLibroMenorOrFalseSiPrecioLibroMayor() {
+        ServicioCrearVenta servicioCrearVenta =  spy(new ServicioCrearVenta(repositorioVenta, repositorioLibro, repositorioUsuario, servicioActualizarLibro));
+        // act - assert
+        assertEquals(servicioCrearVenta.precioLibroOferta(10000F), true);
+        assertEquals(servicioCrearVenta.precioLibroOferta(30000F), false);
     }
 
     @Test
@@ -68,10 +66,6 @@ public class ServicioCrearVentaTest {
     void deberiaCrearVentaDeManeraCorrecta() {
         // arrange
         Venta venta = new VentaTestDataBuilder().build();
-        RepositorioVenta repositorioVenta = Mockito.mock(RepositorioVenta.class);
-        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
-        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
-        ServicioActualizarLibro servicioActualizarLibro = Mockito.mock(ServicioActualizarLibro.class);
         Mockito.when(repositorioUsuario.existePorId(1L)).thenReturn(true);
         Mockito.when(repositorioLibro.existePorId(1L)).thenReturn(true);
         Libro libro = new Libro(1L,"java",10L,25000F);
